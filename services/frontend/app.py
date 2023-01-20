8619fe60ca2479058bb94f35c52e05bd1019f5c4
Jakub Podolak <jp429585@students.mimuw.edu.pl>
2023-01-20 19:37:52 +0100

    review fixes on frontend

--- services/frontend/app.py
+++ services/frontend/app.py
@@ -18,19 +18,6 @@ app = Flask(__name__, static_url_path='')
 # Let's ignore the fact, that secret key is public - for simplicity :)
 app.config['SECRET_KEY'] = "2a7c9ef01952a238eb"
 
-# # REDIS EXAMPLE
-# cache = redis.Redis(host='redis', port=6379)
-# def get_hit_count():
-#     retries = 5
-#     while True:
-#         try:
-#             return cache.incr('hits')
-#         except redis.exceptions.ConnectionError as exc:
-#             if retries == 0:
-#                 raise exc
-#             retries -= 1
-#             time.sleep(0.5)
-
 STATISTICS_URL = 'http://statistics:8080'
 
 def get_available_strategies():
