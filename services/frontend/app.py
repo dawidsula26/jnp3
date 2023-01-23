01c25db70210d2de0ed80ee8727333a9f75e9753
Jakub Podolak <34144008+jjp241@users.noreply.github.com>
2023-01-23 14:08:04 +0100

    Redis cache front statistics (#7)
    * Stupid redis cache
    
    * Select date
    
    Co-authored-by: Jakub Podolak <jp429585@students.mimuw.edu.pl>

--- services/frontend/app.py
+++ services/frontend/app.py
@@ -1,24 +1,36 @@
 import pandas as pd
 import json
+import os
 import plotly
 import plotly.express as px
 import redis
 import random
 import requests
 import yaml
+from dotenv import load_dotenv
 from datetime import datetime
 from flask import Flask,\
                   render_template,\
                   url_for,\
                   request,\
                   redirect
+from flask_caching import Cache  # Import Cache from flask_caching module
+
+load_dotenv()
 
 app = Flask(__name__, static_url_path='')
+app.config.update(
+    CACHE_TYPE = os.environ['CACHE_TYPE'],
+    CACHE_REDIS_HOST = os.environ['CACHE_REDIS_HOST'],
+    CACHE_REDIS_PORT = os.environ['CACHE_REDIS_PORT'],
+    CACHE_REDIS_DB = os.environ['CACHE_REDIS_DB'],
+    CACHE_REDIS_URL = os.environ['CACHE_REDIS_URL'],
+    CACHE_DEFAULT_TIMEOUT = os.environ['CACHE_DEFAULT_TIMEOUT'],
+    STATISTICS_URL = os.environ['STATISTICS_URL'],
+    SECRET_KEY = "2a7c9ef01952a238eb"
+)
 
-# Let's ignore the fact, that secret key is public - for simplicity :)
-app.config['SECRET_KEY'] = "2a7c9ef01952a238eb"
-
-STATISTICS_URL = 'http://statistics:8080'
+cache = Cache(app)  # Initialize Cache
 
 def get_available_strategies():
    f = open('config.yaml')
@@ -32,11 +44,20 @@ def get_variable_values(variable_name,
                         date_begin,
                         date_end):
 
+   date_begin = datetime.strptime(date_begin, '%Y-%m-%d')
+   date_end = datetime.strptime(date_end, '%Y-%m-%d')
+      
    data = {'variableName': {'statistic': 'statT', 'strategy': 'stratT', 'subject': 'subjT'},
-           'startTime': None,
-           'endTime': None}
-   r = requests.post(url = STATISTICS_URL + '/reader/getVariable', json = data) 
-   response = r.json()
+           'startTime': date_begin.strftime('%Y-%m-%dT%H:%M:%SZ'),
+           'endTime': date_end.strftime('%Y-%m-%dT%H:%M:%SZ')}
+
+   response = cache.get(str(data))
+
+   if response is None:        
+      r = requests.post(url = app.config['STATISTICS_URL'] + '/reader/getVariable', json = data) 
+      response = r.json()
+      cache.set(str(data), response)
+
    values = response['values']
    print(values)
 
@@ -51,7 +72,7 @@ def get_variable_values(variable_name,
 @app.route('/', methods=['GET'])
 def index():
    strategies_sets = get_available_strategies()
-   return render_template('index.html', strats=strategies_sets)
+   return render_template('index.html', strats=strategies_sets, date_begin='2023-01-01', date_end='2023-01-07')
 
 
 # specific variable
@@ -74,6 +95,8 @@ def variable():
                                            graphJSON=graphJSON,
                                            strat_name=selected_strategy,
                                            set_name=selected_set,
+                                           date_begin=date_begin,
+                                           date_end=date_end,
                                            strat_desc='DESCRIPTION')
       
 
