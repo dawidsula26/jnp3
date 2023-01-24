20ece855701cba9acd68b5c6c56221b552e5a3dd
Dawid Sula <dawidsula26@gmail.com>
2023-01-24 08:13:24 +0100

    Scalable mongo

--- services/mongo/start.sh
+++ services/mongo/start.sh
@@ -1,4 +1,9 @@
 sleep 15
-
 cd /init
+mongosh "mongodb://configsvr01:27017" < init-configserver.js
+mongosh "mongodb://shard01-a:27017" < init-shard01.js
+mongosh "mongodb://shard02-a:27017" < init-shard02.js
+
+sleep 10
+mongosh "mongodb://mongo:27017" < init-router.js
 mongosh "mongodb://mongo:27017" < init-collection.js
